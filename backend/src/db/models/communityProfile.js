7da459f9f3190c0167bbfddd28834472b66fa558
Alhassan Tijani <jhaeti@gmail.com>
2023-09-08 09:33:45 +0000

    Added field to track poster of a profile

--- backend/src/db/models/communityProfile.js
+++ backend/src/db/models/communityProfile.js
@@ -2,57 +2,63 @@ const mongoose = require("mongoose");
 
 // Creating Item Model
 const communityProfileSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  region: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  district: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  abstract: {
-    type: Buffer,
-    required: true,
-  },
-  mainProfile: {
-    type: Buffer,
-    required: true,
-  },
-  imgs: [
-    {
-      type: Buffer,
-      required: true,
-    },
-  ],
-  imgsNumber: {
-    type: Number,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+	name: {
+		type: String,
+		trim: true,
+		required: true,
+	},
+	region: {
+		type: String,
+		trim: true,
+		required: true,
+	},
+	district: {
+		type: String,
+		trim: true,
+		required: true,
+	},
+	abstract: {
+		type: Buffer,
+		required: true,
+	},
+	mainProfile: {
+		type: Buffer,
+		required: true,
+	},
+	imgs: [
+		{
+			type: Buffer,
+			required: true,
+		},
+	],
+	imgsNumber: {
+		type: Number,
+		required: true,
+	},
+	poster: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "User",
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
 communityProfileSchema.methods.toJSON = function () {
-  const communityProfile = this;
+	const communityProfile = this;
 
-  const communityProfileObject = communityProfile.toObject();
-  delete communityProfileObject.password;
-  delete communityProfileObject.tokens;
+	const communityProfileObject = communityProfile.toObject();
+	delete communityProfileObject.imgs;
+	delete communityProfileObject.abstract;
+	delete communityProfileObject.mainProfile;
 
-  return communityProfile;
+	return communityProfileObject;
 };
 
 const CommunityProfile = mongoose.model(
-  "CommunityProfile",
-  communityProfileSchema
+	"CommunityProfile",
+	communityProfileSchema
 );
 module.exports = CommunityProfile;
