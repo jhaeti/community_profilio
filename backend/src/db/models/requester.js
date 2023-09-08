21f553acca8e754eea6531b0c65520e5e8666753
Alhassan Tijani <jhaeti@gmail.com>
2023-09-08 09:31:56 +0000

    Added a way to get the specific admin to grant a request for a user

--- backend/src/db/models/requester.js
+++ backend/src/db/models/requester.js
@@ -1,35 +1,47 @@
 const mongoose = require("mongoose");
 
 const requesterSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  organization: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  reason: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  communityProfileRequested: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "CommunityProfile",
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+	name: {
+		type: String,
+		trim: true,
+		required: true,
+	},
+	email: {
+		type: String,
+		trim: true,
+		required: true,
+	},
+	organization: {
+		type: String,
+		trim: true,
+		required: true,
+	},
+	reason: {
+		type: String,
+		trim: true,
+		required: true,
+	},
+	communityProfileRequested: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "CommunityProfile",
+	},
+	userToGrant: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "User",
+	},
+	granted: {
+		type: Boolean,
+		default: false,
+	},
+	grantedDate: {
+		type: Date,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
 const Requester = mongoose.model("Requester", requesterSchema);
