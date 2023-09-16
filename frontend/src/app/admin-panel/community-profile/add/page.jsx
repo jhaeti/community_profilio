9fbb97cf901952a4838edfcf5a730968adca2e1d
Alhassan Tijani <jhaeti@gmail.com>
2023-09-16 23:59:09 +0000

    Adding community profile design

---
+++ frontend/src/app/admin-panel/community-profile/add/page.jsx
@@ -0,0 +1,11 @@
+import AddCommunityProfileInput from "./components/AddProfile/AddCommunityProfileInput";
+
+const page = () => {
+	return (
+		<div className="mb-8">
+			<AddCommunityProfileInput />
+		</div>
+	);
+};
+
+export default page;
