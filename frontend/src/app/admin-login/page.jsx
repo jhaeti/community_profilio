44192e896ace85c375f0475b30c649e3f398f64f
Alhassan Tijani <jhaeti@gmail.com>
2023-09-16 23:55:11 +0000

    Admin login page

--- frontend/src/app/admin-login/page.jsx
+++ frontend/src/app/admin-login/page.jsx
@@ -1,5 +1,15 @@
+import TextField from "@mui/material/TextField";
+import styles from "./styles.module.css";
+import AdminLoginForm from "@/components/AdminLoginForm";
 const adminLogin = () => {
-	return <div>Admin Login</div>;
+	return (
+		<>
+			<div className={styles.login_box}>
+				<h2>Login</h2>
+				<AdminLoginForm />
+			</div>
+		</>
+	);
 };
 
 export default adminLogin;
