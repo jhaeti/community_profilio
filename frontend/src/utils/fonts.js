a73c329dcd84288029cb5c312988f404979cc086
Alhassan Tijani <jhaeti@gmail.com>
2023-09-17 00:18:26 +0000

    fonts

---
+++ frontend/src/utils/fonts.js
@@ -0,0 +1,8 @@
+import localFont from "next/font/local";
+
+export const primary = localFont({
+	src: "../../public/fonts/nunito/Nunito-Regular.ttf",
+});
+export const primaryBold = localFont({
+	src: "../../public/fonts/nunito/Nunito-Bold.ttf",
+});
