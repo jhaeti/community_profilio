9acc00254ad285722f89da669b41dca6c5dec951
Alhassan Tijani <jhaeti@gmail.com>
2023-09-17 00:18:01 +0000

    auth hook

---
+++ frontend/src/hooks/useUserContext.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+
+import { UserContext } from "../contexts/UserContext";
+
+const useUserContext = () => {
+	const value = useContext(UserContext);
+	return value;
+};
+
+export default useUserContext;
