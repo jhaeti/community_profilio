601ab1306e40c4f7ce340098ce1b10708954bc17
Alhassan Tijani <jhaeti@gmail.com>
2023-09-17 00:09:37 +0000

    community profile detatils page

--- frontend/src/app/community-profiles/[id]/page.jsx
+++ frontend/src/app/community-profiles/[id]/page.jsx
@@ -1,12 +1,39 @@
 import apiUrl from "@/utils/apiUrl";
 import GoBack from "@/components/GoBack";
+import styles from "./styles.module.css";
+import Btn from "./components/Btn";
+import RequestBtn from "./components/RequestBtn";
+import Pictures from "@/components/MakeRequestForProfile/Pictures";
 
 const CommunityDetails = async ({ params }) => {
 	const res = await fetch(apiUrl + "/community-profiles/" + params.id);
 	const data = await res.json();
+
 	return (
-		<div>
+		<div className="mb-8">
 			<GoBack />
+			<div className={styles.content}>
+				<div className={styles.left}>
+					<Pictures
+						id={params.id}
+						name={data.name}
+						imgsNumber={data.imgsNumber}
+					/>
+				</div>
+				<div className={styles.right}>
+					<p>
+						Community : <strong>{data.name}</strong>
+					</p>
+					<p>
+						District : <strong>{data.district}</strong>
+					</p>
+					<p>
+						Region : <strong>{data.region}</strong>
+					</p>
+					<Btn id={params.id} />
+					<RequestBtn id={params.id} />
+				</div>
+			</div>
 		</div>
 	);
 };
