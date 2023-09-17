8cf9842db73420aa337e24a6c1eeaf5a27909555
Alhassan Tijani <jhaeti@gmail.com>
2023-09-17 00:06:35 +0000

    add user page

---
+++ frontend/src/app/admin-panel/user/add/page.jsx
@@ -0,0 +1,11 @@
+import AddUserProfileInput from "./components/AddUserInputs/AddUserProfileInput";
+
+const page = () => {
+	return (
+		<div className="mb-8">
+			<AddUserProfileInput />
+		</div>
+	);
+};
+
+export default page;
