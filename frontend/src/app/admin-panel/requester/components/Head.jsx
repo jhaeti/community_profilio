132d6dd70e6d99501ebe9e08b9cd4813ca35274e
Alhassan Tijani <jhaeti@gmail.com>
2023-09-26 07:16:46 +0000

    Requester head

---
+++ frontend/src/app/admin-panel/requester/components/Head.jsx
@@ -0,0 +1,17 @@
+import Typography from "@mui/material/Typography";
+const Head = () => {
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "space-between",
+			}}
+		>
+			<Typography mt={2} variant="h4">
+				Requesters
+			</Typography>
+		</div>
+	);
+};
+
+export default Head;
